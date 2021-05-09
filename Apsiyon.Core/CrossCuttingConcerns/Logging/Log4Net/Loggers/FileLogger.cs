4b06e3bc893de16102d7b3da8e3bbb641e6886d6
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-05-09 16:03:21 +0300

    File and Database Logger implementation and LogAspect added.

---
+++ Apsiyon.Core/CrossCuttingConcerns/Logging/Log4Net/Loggers/FileLogger.cs
@@ -0,0 +1,10 @@
+﻿namespace Apsiyon.Core.CrossCuttingConcerns.Logging.Log4Net.Loggers
+{
+    public class FileLogger : LoggerServiceBase
+    {
+        public FileLogger() : base("JsonFileLogger")
+        {
+
+        }
+    }
+}
