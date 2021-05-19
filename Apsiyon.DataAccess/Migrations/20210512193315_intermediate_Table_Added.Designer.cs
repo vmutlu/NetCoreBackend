7848bae596d8f09841f79d271728b70944cf6c47
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-05-19 20:25:47 +0300

    new migration

--- Apsiyon.DataAccess/Migrations/20210512193315_intermediate_Table_Added.Designer.cs
+++ Apsiyon.DataAccess/Migrations/20210512193315_intermediate_Table_Added.Designer.cs
@@ -4,14 +4,16 @@ using Apsiyon.DataAccess.Concrete.EntityFramework.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Apsiyon.DataAccess.Migrations
 {
     [DbContext(typeof(ApsiyonContext))]
-    partial class ApsiyonContextModelSnapshot : ModelSnapshot
+    [Migration("20210512193315_intermediate_Table_Added")]
+    partial class intermediate_Table_Added
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Apsiyon.DataAccess/Migrations/20210512193315_intermediate_Table_Added.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Apsiyon.DataAccess.Migrations
+{
+    public partial class intermediate_Table_Added : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Products");
+
+            migrationBuilder.CreateTable(
+                name: "CategoryWithProducts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryWithProducts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CategoryWithProducts_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CategoryWithProducts_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryWithProducts_CategoryId",
+                table: "CategoryWithProducts",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryWithProducts_ProductId",
+                table: "CategoryWithProducts",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CategoryWithProducts");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
