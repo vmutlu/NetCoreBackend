55574f170473bd7df264e3aecf0dbdb006763452
veysel mutlu <veysel_mutlu42@hotmail.com>
2021-05-19 20:24:49 +0300

    ürün iş kuralı düzenlendi.

--- Apsiyon.Business/ValidationRules/FluentValidation/ProductValidator.cs
+++ Apsiyon.Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -11,10 +11,8 @@ namespace Apsiyon.Business.ValidationRules.FluentValidation
             RuleFor(p => p.ProductName).Length(2,30).WithMessage("Ürün ismi en az 2 en fazla 30 karakterden oluşmalıdır.");
             RuleFor(p => p.UnitPrice).NotEmpty().WithMessage("Ürün fiyatını boş geçemezsiniz");
             RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(1).WithMessage("Ürün fiyatı Minimum 1 TL olmalıdır.");
-            RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(1).When(p=>p.CategoryId == 1);
             RuleFor(p => p.UnitsInStock).NotEmpty().WithMessage("Ürün stok durumu boş geçilemez");
             RuleFor(p => p.QuantityPerUnit).NotEmpty().WithMessage("Ürün birim fiyatını boş geçilemez");
-            RuleFor(p => p.CategoryId).NotEmpty().WithMessage("Ürünü kategorisiz kayıt edemezsiniz");
             //RuleFor(p => p.ProductName).Must(StartWithA);
         }
 
